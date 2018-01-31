95e1dab46ce53ff9557e1823878e4345a9066b31
josealmon10 <josealmon2012@hotmail.com>
2018-01-31 10:54:45 +0100

    GetGenerarLetras

--- JuegoLetras/src/juegoletras/GeneradorLetras.java
+++ JuegoLetras/src/juegoletras/GeneradorLetras.java
@@ -1,4 +1,3 @@
-
 package juegoletras;
 
 public class GeneradorLetras {
@@ -23,7 +22,7 @@ public class GeneradorLetras {
             case 2:
                 let=(char)Math.floor(Math.random()*(90-65)+65);
                 while(true){
-                    if(let=='A' || let=='S' || let=='D' || let=='F' || let=='J' || let=='K' || let=='L' || let=='Ñ'){
+                    if(let=='A' || let=='S' || let=='D' || let=='F' || let=='J' || let=='K' || let=='L' || let=='Ñ' || let=='Q' || let=='W' || let=='O' || let=='P'){
                         break;
                     }else{
                         let=(char)Math.floor(Math.random()*(90-65)+65);
@@ -31,13 +30,37 @@ public class GeneradorLetras {
                 }
                 break;
             case 3:
-                
+                let=(char)Math.floor(Math.random()*(90-65)+65);
+                while(true){
+                    if(let=='A' || let=='S' || let=='D' || let=='F' || let=='J' || let=='K' || let=='L' || let=='Ñ' || let=='Q' || let=='W' || let=='O' || let=='P'  || let=='Z'  || let=='X'  || let=='N' || let=='M'){
+                        break;
+                    }else{
+                        let=(char)Math.floor(Math.random()*(90-65)+65);
+                    }
+                }
                 break;
             case 4:
-                
+                let=(char)Math.floor(Math.random()*(90-65)+65);
+                while(true){
+                    if(let=='A' || let=='S' || let=='D' || let=='F' || let=='J' || let=='K' || let=='L' || let=='Ñ' || let=='Q' || let=='W' || let=='O' || let=='P'  || let=='Z'  || let=='X'  || let=='N' || let=='M' || let=='E' || let=='C' || let=='I' || let=='B'){
+                        break;
+                    }else{
+                        let=(char)Math.floor(Math.random()*(90-65)+65);
+                    }
+                }
                 break;
             case 5:
-            
+                let=(char)Math.floor(Math.random()*(90-65)+65);
+                while(true){
+                    if(let=='A' || let=='S' || let=='D' || let=='F' || let=='J' || let=='K' || let=='L' || let=='Ñ' || let=='Q' || let=='W' || let=='O' || let=='P'  || let=='Z'  || let=='X'  || let=='N' || let=='M' || let=='E' || let=='C' || let=='I' || let=='B' || let=='R' || let=='V' || let=='U' || let=='H'){
+                        break;
+                    }else{
+                        let=(char)Math.floor(Math.random()*(90-65)+65);
+                    }
+                }
+                break;
+            case 6:
+                let=(char)Math.floor(Math.random()*(90-65)+65);
                 break;
         }
         return let;
